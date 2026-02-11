1d82b090fe6c41eddb1b067d01bb28485417f7bf
Millennium Singha <mills9092000@gmail.com>
2026-02-11 14:57:26 +1000

    feat(musical): migrate to net10-winforms and NAudio (closes #9) (#10)

---
+++ src/MusicalInstrument/Form1.cs
@@ -0,0 +1,90 @@
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.Windows.Forms;
+using System.Drawing;
+using System;
+
+namespace MusicalInstrument
+{
+    public partial class Form1 : Form
+    {
+        SignalGenerator sine = new SignalGenerator()
+        {
+            Type = SignalGeneratorType.Sin,
+            Gain = 0.2,
+        };
+
+        WaveOutEvent player = new WaveOutEvent();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try 
+            {
+                player.Init(sine);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing audio: {ex.Message}");
+            }
+
+            trackFrequency.ValueChanged += (s, e) => sine.Frequency = trackFrequency.Value;
+            trackFrequency.Value = 600;
+
+            trackVolume.ValueChanged += (s, e) => player.Volume = trackVolume.Value / 100F;
+            trackVolume.Value = 50;
+
+            // Form closing event to stop player
+            this.FormClosing += (s, e) => 
+            {
+                player.Stop();
+                player.Dispose();
+            };
+        }
+
+        private Point cursorPoritionOnMouseDown;
+        private bool ButtonIsDown = false;
+        private void TheMouseDown(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                // Ignore if audio device fails, just log to console
+                Console.WriteLine($"Audio Error: {ex.Message}");
+            }
+
+            cursorPoritionOnMouseDown = e.Location;
+            ButtonIsDown = true;
+        }
+
+        private void TheMouseUp(object sender, MouseEventArgs e)
+        {
+            player.Stop();
+            ButtonIsDown = false;
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            var dX = cursorPoritionOnMouseDown.X - e.X;
+            var dY = cursorPoritionOnMouseDown.Y - e.Y;
+
+            var vol = player.Volume - (dX / 100000f);
+            var freq = sine.Frequency + (dY / 10f);
+
+            if (ButtonIsDown)
+            {
+                player.Volume = (vol > 0) ? (vol < 1) ? vol : 1 : 0;
+                sine.Frequency = (freq > 100) ? (freq < 1000) ? freq : 1000 : 100;
+
+                trackFrequency.Value = (int)Math.Round(sine.Frequency);
+                trackVolume.Value = (int)Math.Round(player.Volume * 100);
+            }
+
+            Text = $"Musical Instrument! ({dX}, {dY}), ({vol}, {freq})";
+        }
+    }
+}
