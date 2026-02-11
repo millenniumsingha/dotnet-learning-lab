215d87707f20cff01bc5ce2e9bf4f99e1c3c44e1
Millennium Singha <mills9092000@gmail.com>
2026-02-11 15:25:31 +1000

    feat(weather): migrate to net10-winforms (closes #11)

---
+++ src/WeatherTrend/Models/WeatherObservationMetrics.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetLearningLab.WeatherTrend.Models
+{
+    public enum WeatherObservationMetrics
+    {
+        Date_Time,
+        Air_Temp,
+        Barometric_Pressure,
+        Dew_Point,
+        Relative_Humidity,
+        Wind_Dir,
+        Wind_Gust,
+        Wind_Speed
+    }
+}
